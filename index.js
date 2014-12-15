757db09ae68aed32501ae5b5cbd2a06c73033353
Jen <jen@meatspac.es>
2014-12-14 16:24:22 -0800

    initial commit w/ error handling and landing page

---
+++ index.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var Hapi = require('hapi');
+var nconf = require('nconf');
+var Boom = require('boom');
+
+var services = require('./lib/services');
+
+nconf.argv().env().file({ file: 'local.json' });
+
+var server = new Hapi.Server();
+
+server.connection({
+  host: nconf.get('domain'),
+  port: nconf.get('port')
+});
+
+server.views({
+  engines: {
+    jade: require('jade')
+  },
+  isCached: process.env.node === 'production',
+  path: __dirname + '/views',
+  compileOptions: {
+    pretty: true
+  }
+});
+
+var routes = [
+  {
+    method: 'GET',
+    path: '/',
+    config: {
+      handler: services.home
+    }
+  }
+];
+
+server.route(routes);
+
+server.route({
+  path: '/{p*}',
+  method: 'GET',
+  handler: {
+    directory: {
+      path: './public',
+      listing: false,
+      index: false
+    }
+  }
+});
+
+server.ext('onPreResponse', function (request, reply) {
+  var response = request.response;
+
+  if (!response.isBoom) {
+    return reply.continue();
+  }
+
+  var error = response;
+  var ctx = {
+    reason: (error.output.statusCode === 404 ? 'page not found' : 'something went wrong')
+  };
+
+  return reply.view('error', ctx);
+});
+
+server.register({
+  register: require('crumb')
+}, function (err) {
+  if (err) {
+    throw err;
+  }
+});
+
+server.start();
