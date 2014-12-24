04a35ef8771021fe09c13ec65d312c0c2130c249
Jennifer Fong <jen@ednapiranha.com>
2014-12-24 13:15:23 -0800

    js formatting fixup

--- public/js/post.js
+++ public/js/post.js
@@ -3,14 +3,10 @@
 var textarea = document.querySelector('textarea');
 var form = document.querySelector('form');
 
-// submit a post when
-var submitPost = function(e) {
-
-  if (e.keyCode == 13 && (e.metaKey || e.ctrlKey)) {
-
+var submitPost = function (e) {
+  if (e.keyCode === 13 && (e.metaKey || e.ctrlKey)) {
     form.submit();
   }
-
 };
 
 textarea.addEventListener('keydown', submitPost, false);
