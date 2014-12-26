1a33a4bdd211020918b658e8848efa10bdb003da
Richard Torruellas <rtorruellas@gmail.com>
2014-12-26 15:04:54 -0700

    have a limit of 100

---
+++ public/js/global.js
@@ -0,0 +1,26 @@
+'use strict';
+
+(function () {
+  var socket = io();
+  var count = 0;
+  var getChatSessionStorage = window.sessionStorage.getItem('chat');
+  var chatArr = [];
+
+  if (getChatSessionStorage){
+    JSON.parse(getChatSessionStorage).forEach( function (data) {
+      chatArr.push(data);
+      count ++;
+      if (count > 100) {
+        count --;
+        chatArr.shift();
+      }
+    });
+  }else {
+    window.sessionStorage.setItem('chat', JSON.stringify(chatArr));
+  }
+
+  socket.on('message', function (data) {
+    chatArr.push(data);
+    window.sessionStorage.setItem('chat', JSON.stringify(chatArr));
+  });
+})();
