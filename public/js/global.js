f68d1ccf406d4205ff96768263d0ac3b4a04df14
Colin Gourlay <Gourlay.Colin@abc.net.au>
2015-01-08 10:42:24 +1000

    Replaced some custom date formatting stuff with native Date instance methods.

--- public/js/global.js
+++ public/js/global.js
@@ -2,28 +2,17 @@
 'use strict';
 
 (function () {
-  var MONTHS = 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split('_');
+  var DAY = /^\w+\s/;
+  var MINS_SECS = /(:\d+):\d+/;
   var socket = io();
   var count = 0;
   var getChatSessionStorage = window.sessionStorage.getItem('chat');
   var chatArr = [];
   var timeEls = document.querySelectorAll('time[datetime]');
-  var ordinal = function (number) {
-      var b = number % 10,
-          output = (+(number % 100 / 10) === 1) ? 'th' :
-          (b === 1) ? 'st' :
-          (b === 2) ? 'nd' :
-          (b === 3) ? 'rd' : 'th';
-      return number + output;
-  };
-  var zeroFill = function (number) {
-    return ((number < 10) ? '0' : '') + number;
-  };
   var localDate = function (timestamp) {
-    var d = new Date(timestamp);
-    var hour = d.getHours();
-    return MONTHS[d.getMonth()] + ' ' + ordinal(d.getDate()) + ', ' + d.getFullYear() + ' - ' +
-      (hour % 12 || 12) + ':' + zeroFill(d.getMinutes()) + ' ' + (hour % 12 === hour ? 'am' : 'pm');
+    var date = new Date(timestamp);
+    return date.toDateString().replace(DAY, '') + ' - ' +
+      date.toLocaleTimeString().replace(MINS_SECS, '$1').toLowerCase();
   };
 
   if (getChatSessionStorage){
